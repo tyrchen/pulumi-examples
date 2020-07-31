704dc85da9ca7866cc01e02693adf39bb8dfba46
Tyr Chen <tyr.chen@gmail.com>
2020-07-30 23:45:07 -0700

    ignore processed files

--- image-resize/index.ts
+++ image-resize/index.ts
@@ -13,8 +13,8 @@ bucket.onObjectCreated("onImgUploaded", async bucketArgs => {
 
     const s3 = new aws.sdk.S3();
     for (const rec of bucketArgs.Records) {
-
         const [buck, key] = [rec.s3.bucket.name, rec.s3.object.key];
+        if (key.startsWith("processed")) continue;
         console.log(`Processing: file ${key}`);
         const data = await s3.getObject({ Bucket: buck, Key: key }).promise();
         if (!data.Body) continue;
