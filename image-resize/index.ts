f2c486e2fcdd0463d903f49f8b5c5ab5ea5dcbff
Tyr Chen <tyr.chen@gmail.com>
2020-07-31 19:22:30 -0700

    use attention resize

--- image-resize/index.ts
+++ image-resize/index.ts
@@ -1,6 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 import * as sharp from "sharp";
 
 const bucket = new aws.s3.Bucket("images");
@@ -9,6 +7,8 @@ export const bucketName = bucket.id;
 
 bucket.onObjectCreated("onImgUploaded", async bucketArgs => {
     console.log("onImgUploaded called");
+    const width = 240;
+    const height = 240;
     if (!bucketArgs.Records) return;
 
     const s3 = new aws.sdk.S3();
@@ -16,10 +16,19 @@ bucket.onObjectCreated("onImgUploaded", async bucketArgs => {
         const [buck, key] = [rec.s3.bucket.name, rec.s3.object.key];
         if (key.startsWith("processed")) continue;
         console.log(`Processing: file ${key}`);
+
         const data = await s3.getObject({ Bucket: buck, Key: key }).promise();
         if (!data.Body) continue;
 
-        const result = await sharp(data.Body as Buffer).resize(240, 240).jpeg().toBuffer();
+        const result = await sharp(data.Body as Buffer)
+            .resize({
+                width,
+                height,
+                fit: sharp.fit.cover,
+                position: sharp.strategy.attention,
+            })
+            .jpeg()
+            .toBuffer();
         const newFile = `processed/${key}`;
         await s3.putObject({
             Bucket: buck,
